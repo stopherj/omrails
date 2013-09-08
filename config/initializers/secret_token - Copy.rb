2e0d7632582a6deb464266c77f0ff3d6a82db40d
Chris Johnson <christophj@gmail.com>
2013-09-08 01:49:51 -0700

    change gemlock

---
+++ config/initializers/secret_token - Copy.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'da58e4adee5c98974bbde54f167474ca0aa3ce8d5dc977c32d29906b62e616618d61153f7794551f6f2cc5056c3c4b6ad03cfc71e4dd5fbf7b233ee7d001fd07'
